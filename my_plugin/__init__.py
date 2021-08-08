0bfdaff7dd81575889e424ec6c751c28dcc8919b
Fallen_Breath <yqx791125833@gmail.com>
2021-08-08 09:54:45 +0800

    use space char

--- my_plugin/__init__.py
+++ my_plugin/__init__.py
@@ -4,5 +4,5 @@ from my_plugin import my_lib
 
 
 def on_load(server: PluginServerInterface, old):
-	server.logger.info(server.tr('my_plugin.a_message'))
-	my_lib.do_something()
+    server.logger.info(server.tr('my_plugin.a_message'))
+    my_lib.do_something()
--- my_plugin/my_lib.py
+++ my_plugin/my_lib.py
@@ -1,2 +1,2 @@
 def do_something():
-	pass
+    pass
